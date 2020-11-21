bf91b6769afb859b3989824c2b8bf108b037d8e8
pablodgs <37851924+pablodgs@users.noreply.github.com>
2020-11-21 09:41:58 -0300

    Add files via upload

---
+++ stweb-v1/javascript/registrar.js
@@ -0,0 +1,8 @@
+function mostrarFisico(){
+    document.getElementById('user-fisico').style.visibility = 'visible';
+    document.getElementById('user-juridico').style.visibility = 'hidden';
+}
+function mostrarJuridico(){
+    document.getElementById('user-juridico').style.visibility = 'visible';
+    document.getElementById('user-fisico').style.visibility = 'hidden';
+}
