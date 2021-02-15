613ad8ecb9fc9c32e1b8de06f22d4e76a037e88d
José Vitor <43215770+DoidoAt@users.noreply.github.com>
2021-02-15 13:37:12 -0300

    Delete stweb-v1 directory

--- stweb-v1/javascript/registrar.js
+++
@@ -1,8 +0,0 @@
-function mostrarFisico(){
-    document.getElementById('user-fisico').style.visibility = 'visible';
-    document.getElementById('user-juridico').style.visibility = 'hidden';
-}
-function mostrarJuridico(){
-    document.getElementById('user-juridico').style.visibility = 'visible';
-    document.getElementById('user-fisico').style.visibility = 'hidden';
-}
